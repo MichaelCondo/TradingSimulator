e2bc2dfb6b8206d691549d2d7f36902fb51ccd9b
MichaelCondo <michael.condo@mail.utoronto.ca>
2020-04-26 16:50:56 -0400

    Added side bar navigation in the application html file, only if the user is signed in.Also fixed bug that displayed premature logging in error message.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,10 @@ class ApplicationController < ActionController::Base
   def require_valid_user!
     if current_user.nil?
       flash[:error] = 'You must be logged in to access that page!'
+      @signed_in = false
       redirect_to login_path
+    else
+      @signed_in = true
     end
   end
 end
