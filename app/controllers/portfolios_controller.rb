5c5de52b3b0d036f2f0e0e5dd0815c6206779821
MichaelCondo <michael.condo@mail.utoronto.ca>
2020-05-01 21:01:58 -0400

    Using same API endpoint for pulling current price in portfolio controller, just for consistency and more up-to-date prices.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,19 +9,18 @@ class PortfoliosController < ApplicationController
       @port_inv_value = 0.0
       @book_cost = 0.0
       @current_user.portfolio.portfolio_stocks.each do |stock|
-        @response = HTTParty.get('https://financialmodelingprep.com/api/v3/stock/real-time-price/' + stock.ticker)
-        stock.current_price = @response.parsed_response['price']
-        stock.market_value = @response.parsed_response['price'] * stock.quantity
-        stock.gain_loss = (@response.parsed_response['price'] * stock.quantity) - stock.book_cost
+        @response = HTTParty.get('https://financialmodelingprep.com/api/v3/quote/' + stock.ticker)
+        stock.current_price = @response.parsed_response.first['price']
+        stock.market_value = @response.parsed_response.first['price'] * stock.quantity
+        stock.gain_loss = (@response.parsed_response.first['price'] * stock.quantity) - stock.book_cost
         stock.save!
-        @port_inv_value = @port_inv_value + (@response.parsed_response['price'] * stock.quantity)
+        @port_inv_value = @port_inv_value + (@response.parsed_response.first['price'] * stock.quantity)
         @book_cost = @book_cost + stock.book_cost
       end
 
       @current_user.portfolio.investment_value = @port_inv_value
       @current_user.portfolio.book_cost = @book_cost
-      @current_user.portfolio.cash = 100000 - @book_cost
-      @current_user.portfolio.portfolio_value = @port_inv_value + (100000 - @book_cost)
+      @current_user.portfolio.portfolio_value = @port_inv_value + @current_user.portfolio.cash
       @current_user.portfolio.gain_loss = @port_inv_value - @book_cost
       @current_user.portfolio.save!
     end
