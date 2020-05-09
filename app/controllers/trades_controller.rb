0f5837066b52f03f94b026c8b79fcb075f526cb7
MichaelCondo <michael.condo@mail.utoronto.ca>
2020-05-09 14:09:27 -0400

    Added error handling for searching to flash an error if ticker doesn not exist.

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -5,11 +5,16 @@ class TradesController < ApplicationController
     if params[:search].blank?
       @parameter = ''
       session[:stock_parameter] = @parameter
+      @response = 'placeholder'
     else
       @parameter = params[:search].upcase
       session[:stock_parameter] = @parameter
       @response = HTTParty.get('https://financialmodelingprep.com/api/v3/quote/' + @parameter.to_s)
-      session[:parsed_response] = @response.parsed_response.first
+      if @response.present?
+        session[:parsed_response] = @response.parsed_response.first
+      else
+        flash[:error] = 'That ticker does not exist'
+      end
     end
   end
 
